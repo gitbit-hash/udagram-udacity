a2cfe21461e7f142582f6d20e48a45c6c206a52f
unknown <brokedeveloperhelp@gmail.com>
2022-10-31 00:37:52 +0200

    feat: Configuring Elastick Beanstack and successfully upload Node Express server

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -2,10 +2,10 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
 import { sequelize } from "./sequelize";
-
-import { IndexRouter } from "./controllers/v0/index.router";
-
 import bodyParser from "body-parser";
+
+import { config } from "./config/config";
+import { IndexRouter } from "./controllers/v0/index.router";
 import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
 (async (): Promise<void> => {
@@ -26,7 +26,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 	console.log("Database Connected");
 
 	const app = express();
-	const port = 8080;
+	const port = config.port;
 
 	app.use(bodyParser.json());
 
